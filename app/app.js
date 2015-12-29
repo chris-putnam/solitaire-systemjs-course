98d23c6955082c9e7ff32fdd0c2a17961d34f60e
Chris Putnam <chris@christopher-putnam.com>
2015-12-28 19:16:18 -0500

    organize code into klondike module

--- app/app.js
+++ app/app.js
@@ -1,7 +1,4 @@
-import 'klondike/scoring.js';
 import 'klondike/klondike.js';
-import 'klondike/board.js';
-import 'klondike/game.js';
 
 angular.module("solitaire", ["klondike", "ngDraggable"]);
 
--- app/klondike/klondike.js
+++ app/klondike/klondike.js
@@ -1,3 +1,10 @@
+import { Scoring } from 'klondike/scoring.js';
+import 'klondike/board.js';
+import 'klondike/game.js';
+
+angular.module("klondike.scoring", [])
+  .service("scoring", [Scoring]);
+
 angular.module("klondike", [
   "klondike.game",
   "klondike.board",
